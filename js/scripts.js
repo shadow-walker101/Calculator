059dcce8d4ce2881869e1c6cb7b5d0c9b545bf31
shadow-walker101 <otienokarani019@gmail.com>
2021-07-07 11:55:51 +0300

    Adds an addition function for any two numbers

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+let number1 = parseInt(prompt("Enter a number:"));
+let number2 = parseInt(prompt("Enter another number:"));
+let result = add(number1, number2);
+alert(result);
