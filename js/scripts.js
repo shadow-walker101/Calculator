aac2c007c1353cd1392521b3312e0d6f80fb6a58
shadow-walker101 <otienokarani019@gmail.com>
2021-07-09 18:38:34 +0300

    Adds the altnaertive logic that supports other operations

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,84 @@
 // Business (or back-end) logic:
 
-let add = (number1, number2) => {
-    return number1 + number2;
-};
+// let add = (number1, number2) => {
+//     return number1 + number2;
+// };
 
-let subtract = (number1, number2) => {
-    return number1 - number2;
-};
+// let subtract = (number1, number2) => {
+//     return number1 - number2;
+// };
 
 
-let multiply = (number1, number2) => {
-    return number1 * number2;
-};
+// let multiply = (number1, number2) => {
+//     return number1 * number2;
+// };
 
-let divide = (number1, number2) => {
-    return number1 / number2;
-};
+// let divide = (number1, number2) => {
+//     return number1 / number2;
+// };
 
-$(document).ready(() => {
-    $("form#add").submit((event) => {
+// $(document).ready(() => {
+//     $("form#add").submit((event) => {
+//         event.preventDefault();
+//         let number1 = parseInt($("#add1").val());
+//         let number2 = parseInt($("#add2").val());
+//         let result = add(number1, number2);
+//         $("#output").text(result)
+//     });
+// });
+
+//Alternative code
+
+
+let form = document.getElementById("add")
+
+function addition() {
+    form.addEventListener('submit', (event) => {
         event.preventDefault();
         let number1 = parseInt($("#add1").val());
         let number2 = parseInt($("#add2").val());
-        let result = add(number1, number2);
-        $("#output").text(result);
-    });
-});
+        let result = number1 + number2;
+        $("#output").text(result)
+
+    })
+
+}
+
+
+function subtraction() {
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let number1 = parseInt($("#add1").val());
+        let number2 = parseInt($("#add2").val());
+        let result = number1 - number2;
+        $("#output").text(result)
+
+    })
+
+}
+
+
+function multiply() {
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let number1 = parseInt($("#add1").val());
+        let number2 = parseInt($("#add2").val());
+        let result = number1 * number2;
+        $("#output").text(result)
+
+    })
+
+}
+
+
+function division() {
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let number1 = parseInt($("#add1").val());
+        let number2 = parseInt($("#add2").val());
+        let result = number1 / number2;
+        $("#output").text(result)
+
+    })
+
+}
